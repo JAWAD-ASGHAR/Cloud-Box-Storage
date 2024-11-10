3e498ca88ab273592f2f04a9f26e15c345e7972b
JAWAD-ASGHAR <jawadasghar263@gmail.com>
2024-11-10 17:50:56 +0500

    not found page redesign

--- app/not-found.js
+++ app/not-found.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>Page not found</div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">404</h1>
+        <p className="text-2xl">Page not found</p>
+        <div className="mt-10">
+          <a
+            href="/"
+            className="bg-primary-500 hover:bg-primary-600 transition-all ease-in-out duration-300 text-white p-3 rounded-lg"
+          >
+            Go back home
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default NotFound;
+
