6184b8827bc498230ef8613cf55a47ad042a9430
JAWAD-ASGHAR <jawadasghar263@gmail.com>
2024-11-10 19:07:47 +0500

    Dashboard flex fixed

--- Providers/ChildrenProvider.js
+++ Providers/ChildrenProvider.js
@@ -9,23 +9,23 @@ const ChildrenProvider = ({ children }) => {
   const { data: session, status } = useSession();
 
   return (
-    <div className="flex">
+    <>
       {status === "authenticated" && session ? (
         <>
-          <SideNavBar />
-          <div className="grid grid-cols-1 md:grid-cols-3 w-full">
-            {/* Children container spanning two columns on medium screens and up */}
-            <div className="col-span-2">{children}</div>
-            {/* Storage container spanning one column */}
-            <div className="order-first md:order-last">
-              <Storage />
+          <div className="flex">
+            <SideNavBar />
+            <div className="grid grid-cols-1 md:grid-cols-3 w-full">
+              <div className="col-span-2">{children}</div>
+              <div className="order-first md:order-last">
+                <Storage />
+              </div>
             </div>
           </div>
         </>
       ) : (
         status !== "loading" && children
       )}
-    </div>
+    </>
   );
 };
 
